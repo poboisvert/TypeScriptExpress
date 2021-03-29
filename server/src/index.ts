e6544635b0b821fcc42981a69295c33fc02dd71c
Pierre-Olivier Boisvert <pob944@hotmail.com>
2021-03-29 17:42:51 -0400

    Routing Index P:3000

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,19 @@
-console.log("TS INIT")
+console.log("TS INIT");
+
+import express, { Response, Request } from "express";
+
+const app = express(); // Init middleware
+const PORT = 3000;
+// ROUTING
+app.get("/", (req: Request, res: Response) => {
+  console.log("Home / ");
+
+  res.send(`
+    <div>
+    Home
+    </div>`);
+});
+
+app.listen(PORT, () => {
+  console.log(`àctive on ${PORT}`);
+});
