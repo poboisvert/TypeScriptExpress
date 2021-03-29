47382b99795ec5082cfad9bd0615900750fe22a4
Pierre-Olivier Boisvert <pob944@hotmail.com>
2021-03-29 19:04:31 -0400

    Signout TS

--- server/src/routes/loginRoutes.ts
+++ server/src/routes/loginRoutes.ts
@@ -24,15 +24,49 @@ router.get("/login", (req: Request, res: Response) => {
   </form>
   `);
 
+  // ROUTE - /login
+
   router.post("/login", (req: RequestWithBody, res: Response) => {
     const { email, password } = req.body;
     // res.send(email + password);
     if (email && password && email === "test@test.com" && password === "test") {
-      res.send(email.toUpperCase()); // Need type guard
+      // Create the session cookie
+      req.session = { loggedIn: true, name: "test" };
+
+      // Redirect
+      res.redirect("/");
     } else {
       res.send("Invalid email");
     }
   });
 });
 
+// ROUTE - /
+router.get("/", (req: Request, res: Response) => {
+  //Confirm if session is saved
+  if (req.session && req.session.loggedIn) {
+    // req.session.loggedI RS confirm undefined
+    res.send(`
+    <div>
+    <h3>Hi ${req.session.name}</h3>
+    
+    <a href="/logout">Sign Out</a>
+    </div>`);
+  } else {
+    res.send(`
+    <div>
+        <h3>Please Sign In</h3>
+       <a href="/login">Sign In</a>
+    </div>`);
+  }
+  }
+});
+
+// ROUTE - /logout
+router.get("/logout", (req: Request, res: Response) => { 
+    // Reset session
+    req.session = undefined
+    // Redirect
+    res.redirect("/");
+}
 export { router }; // {} Export a variable already declared
